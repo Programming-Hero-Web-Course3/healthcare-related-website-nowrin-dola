cc61537b1009e181bbde31c7f15a265ee96ebc44
nowrin-dola <nowrinsiddiquedola@gmail.com>
2021-10-20 23:47:02 +0600

    added content

--- src/Pages/Doctors/BookApoinment/BookAppoinment.js
+++ src/Pages/Doctors/BookApoinment/BookAppoinment.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router';
-import pic from '../../../images/book Doctor/2053077.jpg'
+import pic from '../../../images/book Doctor/2053077.jpg';
+import './BookAppoinment.css'
 
 const BookAppoinment = () => {
     const { id } = useParams();
     return (
         <div className='m-5 text-center'>
-            <img src={pic} style={{ width: '350px', height: '300px' }} alt="" srcset="" />
+            <img src={pic} className='book-pic' alt="" srcset="" />
             <h3 className='mt-3 text-danger'>you booked your appoinment for Dr. {id}</h3>
 
         </div>
